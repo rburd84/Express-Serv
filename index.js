be0d1e898a50c551459157015dad5279757eba0d
rburd84 <developer.rburden@gmail.com>
2021-02-27 02:34:29 -0500

    server online

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+// const home = require('./')
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send(`It's lit Im figuring it out!!`)
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
